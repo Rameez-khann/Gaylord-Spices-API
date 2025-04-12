60f1a49432313a30acf4c42296cf3c722bec6a3a
Rameez Khann <victor@MacBook-Air.local>
2025-04-12 14:12:04 +0300

    Created a filter to ensure deleted records are not accidentally seen by the user

---
+++ js/Filter.js
@@ -0,0 +1,29 @@
+const FirebaseClient = require("../firebase/firebase-client");
+
+class Filter {
+  collection = "filtered-recipes";
+  filterId = "filtered-recipes";
+  dbClient = new FirebaseClient(this.collection);
+
+  async getFilteredRecipes() {
+    const item = await this.get(this.filterId);
+    return item?.items || [];
+  }
+
+
+  async get(id) {
+    return this.dbClient.get(id);
+  }
+
+
+  async updateFilteredRecipes(idMeal) {
+    const idMeals = await this.getFilteredRecipes();
+    if (!idMeals.includes(idMeal)) {
+      idMeals.push(idMeal);
+      const save = await this.update(this.filterId, { items: idMeals });
+      return save;
+    } else {
+      return null;
+    }
+  }
+}
